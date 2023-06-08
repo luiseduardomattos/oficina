65fec152c531ae716a55a23305098e2d6ce195dc
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2023-06-07 22:20:49 -0300

    .

--- Aula06/clientes-salvar.php
+++ Aula06/clientes-salvar.php
@@ -2,7 +2,7 @@
 // receber as informações
 
 $nome = $_POST["nome"];
-
+$telefone = $_POST["telefone"];
 // dados da conexão
 
 $bd_usuario = "root";
@@ -16,7 +16,7 @@ $conexao = mysqli_connect($bd_servidor, $bd_usuario, $bd_senha, $_bd_nome_do_ban
 
 // criar o sql inserir
 
-$sql = "insert into t_clientes(nome) values('$nome')";
+$sql = "insert into t_clientes(nome, telefone) values('$nome', '$telefone')";
 
 // executar o sql no banco
 
